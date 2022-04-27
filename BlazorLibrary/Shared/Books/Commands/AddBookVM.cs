5c99f7c1f3a899025ccfeef8f7e7b826d32cd1a3
Dorian Margraf <dorian.margraf@gmail.com>
2022-04-27 19:49:59 +0200

    Add project files.

---
+++ BlazorLibrary/Shared/Books/Commands/AddBookVM.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorLibrary.Shared.Books.Commands
+{
+    public class AddBookVM
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public WriterVm Writers { get; set; }
+        public TypeVm Types { get; set; }
+        public string Publisher { get; set; }
+        public DateTime DateOfEdition { get; set; }
+        public int NumberOfPages { get; set; }
+        public string ISBN { get; set; }
+        public bool Available { get; set; }
+    }
+    public class AddBookValidator : AbstractValidator<AddBookVM>
+    {
+        public AddBookValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().MaximumLength(15).WithMessage("Title must be max 15 characters length");
+            RuleFor(x => x.Description).NotEmpty().MaximumLength(30).WithMessage("Description must be max 30 characters length");
+            RuleFor(x => x.Publisher).NotEmpty().MaximumLength(20).WithMessage("Publisher must be max 20 characters length");
+            
+        }
+    }
+
+    public class TypeVm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class WriterVm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    
+}
---
+++ BlazorLibrary/Shared/Users/Commands/AddUserVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace BlazorLibrary.Shared.Users.Commands
+{
+    public class AddUserVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Required]
+        [PhoneAttribute]
+        [MaxLength(12, ErrorMessage ="Phone number is to long")]
+        public string PhoneNumber { get; set; }
+        public string EmailAdress { get; set; }
+    }
+
+    public class AddUserValidator : AbstractValidator<AddUserVM>
+    {
+        public AddUserValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(15).WithMessage("Name must be max 15 characters length");
+            RuleFor(x => x.EmailAdress).NotEmpty().EmailAddress();
+        }
+    }
+}
